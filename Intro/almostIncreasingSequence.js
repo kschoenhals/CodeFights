bea73d1326c3c2e8fbd04e8c3bbef5d8b9b787ae
Kyle Schoenhals <37464207+kschoenhals@users.noreply.github.com>
2018-04-03 20:41:10 -0500

    Update almostIncreasingSequence.js

--- Intro/almostIncreasingSequence.js
+++ Intro/almostIncreasingSequence.js
@@ -1,5 +1,17 @@
 // determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array
 
 function almostIncreasingSequence(sequence) {
+    
+    var count = 0;
+    
+    for(i = 0; i < sequence.length; i++) {
+        if(sequence[i] <= sequence[i-1]) {
+            count++;
+            if(sequence[i] <= sequence[i-2] && sequence[i+1] <= sequence[i-1]) {
+                return false;
+            }
+        }
+    }
+    return count <= 1;
 
 }
