5e3488e5204a2889a6945b0815747f3d9494a2e0
Kyle Schoenhals <37464207+kschoenhals@users.noreply.github.com>
2018-04-06 11:29:16 -0500

    Update allLongestStrings.js

--- Intro/allLongestStrings.js
+++ Intro/allLongestStrings.js
@@ -1,5 +1,18 @@
 // Given an array of strings, return another array containing all of its longest strings
 
 function allLongestStrings(inputArray) {
-
+    
+    var longest = inputArray[0].length;
+    
+    for(var i = 0; i < inputArray.length; i++) {
+        
+        if(longest < inputArray[i].length) {
+            longest = inputArray[i].length;
+        }
+    }
+    
+    inputArray = inputArray.filter((element)=> {
+        return element.length === longest;
+    });
+    return inputArray;
 }
